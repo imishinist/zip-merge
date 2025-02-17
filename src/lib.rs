7b724afff9adc62ec39165bb96d6b89836aa684c
Taisuke Miyazaki <imishinist@gmail.com>
2025-02-17 23:22:55 +0900

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::collections::BTreeMap;
-use std::io::{Read, Seek, Write};
+use std::io::{self, Read, Seek, Write};
 use std::path::PathBuf;
+
 use zip::write::{ExtendedFileOptions, FileOptions};
 use zip::{CompressionMethod, ZipArchive, ZipWriter};
 
@@ -15,13 +16,11 @@ struct Index {
 fn default_file_selector(indices: &[Index]) -> Index {
     assert!(!indices.is_empty());
 
-    let mut max = indices[0];
-    for index in indices {
-        if index.archive_idx > max.archive_idx {
-            max = *index;
-        }
-    }
-    max
+    indices
+        .iter()
+        .max_by_key(|&index| index.archive_idx)
+        .unwrap()
+        .clone()
 }
 
 pub struct ZipMerger<R> {
@@ -44,12 +43,12 @@ impl<R: Read + Seek> ZipMerger<R> {
         let archive_idx = self.archives.len();
 
         let mut archive = ZipArchive::new(input)?;
-        for i in 0..archive.len() {
-            let file = archive.by_index(i)?;
+        for file_idx in 0..archive.len() {
+            let file = archive.by_index(file_idx)?;
             if let Some(path) = file.enclosed_name() {
-                self.files.entry(path).or_insert(Vec::new()).push(Index {
+                self.files.entry(path).or_insert_with(Vec::new).push(Index {
                     archive_idx,
-                    file_idx: i,
+                    file_idx,
                 });
             }
         }
@@ -66,17 +65,12 @@ impl<R: Read + Seek> ZipMerger<R> {
             .unix_permissions(0o644);
         for (key, value) in self.files.iter() {
             writer.start_file_from_path(key, opt.clone())?;
-            let index = if value.len() == 1 {
-                value[0]
-            } else {
-                (self.file_selector)(value.as_slice())
-            };
 
+            let index = (self.file_selector)(value);
             let archive = &mut self.archives[index.archive_idx];
             let mut file = archive.by_index(index.file_idx)?;
-            let mut buf = Vec::new();
-            file.read_to_end(&mut buf)?;
-            writer.write_all(&buf)?;
+
+            io::copy(&mut file, &mut writer)?;
         }
         writer.finish()?;
 
